dd563b82c31e5962def48c3bdadeb784056ef433
sandeep <spatlemail@gmail.com>
2019-02-14 02:25:18 +0100

    Task - Add attributes and initialize user. Make test green

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,3 +1,9 @@
 class User
-  
+  attr_accessor :id, :name, :html_url
+
+  def initialize(params)
+    params.each do |k,v|
+      instance_variable_set("@#{k}",v) unless v.nil?
+    end
+  end
 end
